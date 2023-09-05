0d229ee10ddd95c371ecb5823d24e8463f346eca
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-09-05 18:27:05 +0200

    sidebar right

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -78,6 +78,36 @@ const Sidebar: React.FC<SidebarProps> = ({
       <main className="h-full flex-1 overflow-y-auto py-2">
         {children}
       </main>
+      <div
+        className="
+          hidden
+          md:flex
+          flex-col
+          gap-y-2
+          bg-black
+          h-full
+          w-[300px]
+          p-2
+        "
+        >
+          <Box>
+            <div className="
+              flex
+              flex-col
+              gap-y-4
+              px-5
+              py-4
+            ">
+              {routes.map((item) => (
+                <SidebarItem key={item.label} {...item} />
+
+              ))}
+            </div>
+          </Box>
+          <Box className="overflow-y-auto h-full">
+            <Library songs={songs} />
+          </Box>
+      </div>
     </div>
   );
 }
