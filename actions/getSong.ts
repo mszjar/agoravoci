8c7800276d31ef6bddf9f0e3d3803b81c1ceffc8
ravield <ravield1192@gmail.com>
2023-09-12 18:35:39 -0300

    fix conflicts on merge

--- actions/getSong.ts
+++ actions/getSong.ts
@@ -10,13 +10,9 @@ const getSong = async (id: string): Promise<Song> => {
   const { data, error } = await supabase
     .from("songs")
     .select()
-<<<<<<< HEAD
     .eq("id", `${id}`)
     .limit(1)
     .single();
-=======
-    .eq("id", `${id}`);
->>>>>>> 41a69eacea808873ab6d22032b4368263cf5468d
 
   if (error) {
     console.log(error.message);
