9c69ee7a28a713b0bfd203bb5259525ce4944ade
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-09-17 19:33:26 +0200

    landing page

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -3,6 +3,8 @@
 import { Song } from "@/types";
 import SongItem from "@/components/SongItem";
 import useOnPlay from "@/hooks/useOnPlay";
+import { useUser } from "@/hooks/useUser";
+
 
 interface PageContentProps {
   songs: Song[];
@@ -12,34 +14,62 @@ const PageContent: React.FC<PageContentProps> = ({
   songs
 }) => {
   const onPlay = useOnPlay(songs);
+  const { user } = useUser();
 
   if (songs.length === 0) {
     return (
       <div className="mt-4 text-neutral-400">
-        No songs available.
+        No posts available.
       </div>
     )
   }
   return (
-    <div className="
-      grid
-      grid-cols-2
-      sm:grid-cols-3
-      lg:grid-cols-4
-      xl:grid-cols-5
-      2xl:grid-cols-8
-      gap-4
-      mt-4
-      mb-32
-    ">
-      {songs.map((item) => (
-        <SongItem
-          key={item.id}
-          onClick={(id: string) => onPlay(id)}
-          data={item}
-        />
-      ))}
-    </div>
+    <>
+    	{ user ? (
+        <div className="
+          grid
+          grid-cols-2
+          sm:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-8
+          gap-4
+          mt-4
+          mb-32
+        ">
+          {songs.map((item) => (
+            <SongItem
+              key={item.id}
+              onClick={(id: string) => onPlay(id)}
+              data={item}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="gap-x-4 items-center">
+          <p>Not connected</p>
+          <div className="
+            grid
+            grid-cols-2
+            sm:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-8
+            gap-4
+            mt-4
+            mb-32
+          ">
+            {songs.slice(0, 2).map((item) => (
+              <SongItem
+                key={item.id}
+                onClick={(id: string) => onPlay(id)}
+                data={item}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
    );
 }
 
